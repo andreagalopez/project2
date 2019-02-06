cb62c8998a54de4bc22c75ea1207b55cfd11cf85
Miguel Elizondo <miguelelizondov@gmail.com>
2019-02-05 19:34:12 -0600

    Api-Routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,15 +2,25 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/user/:id", function(req, res) {
+    db.User.findOne({
+      where: {
+        id = req.params.id
+      },
+      include: [_Event]
+    }).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/event", function(req, res) {
+    db._Event.create({
+      name: req.body.name,
+      date: req.body.date,
+      time: req.body.time,
+      place: req.body.place
+    }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
@@ -21,4 +31,21 @@ module.exports = function(app) {
       res.json(dbExample);
     });
   });
+
+  app.put("/api/event/:email",function(req,res){
+    db.EventGuestList.update(
+      {
+      confirm: req.body.confirm
+      },
+     { 
+      where:
+      {
+        guest_email: req.params.email
+      }
+    }).then(function(dbPost){
+      res.json(dbPost);
+    });
+  });
 };
+
+
