22efbd9e44054f0a506c9ea4dbc3b3cc7aac7953
gabrieldiaz23 <pacho.rudo22@gmail.com>
2019-02-07 18:49:02 -0600

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,27 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+var name = $("#txtFirstName");
+var email = $("#txtEmail");
+var password = $("#txtPassword");
+var submitBtn = $("#submitBtn");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  insertUser: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "api/examples",
+      url: "/signup",
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
@@ -64,7 +63,7 @@ var refreshExamples = function() {
 var handleFormSubmit = function(event) {
   event.preventDefault();
 
-  var example = {
+/*   var example = {
     text: $exampleText.val().trim(),
     description: $exampleDescription.val().trim()
   };
@@ -72,14 +71,13 @@ var handleFormSubmit = function(event) {
   if (!(example.text && example.description)) {
     alert("You must enter an example text and description!");
     return;
-  }
+  } */
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
+  API.insertUser(example);
 
-  $exampleText.val("");
-  $exampleDescription.val("");
+  name.val("");
+  email.val("");
+  password.val("");
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
@@ -95,5 +93,5 @@ var handleDeleteBtnClick = function() {
 };
 
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+submitBtn.on("click", handleFormSubmit);
+//$exampleList.on("click", ".delete", handleDeleteBtnClick);
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,19 +7,52 @@ module.exports = function(app) {
     res.render("panelcontrol");
   });
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index1", {
+  app.get("/", function (req, res) {
+    console.log(req.body);
+    db.User.findAll({}).then(function (dbExamples) {
+      res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
       });
     });
   });
 
-  /*After the login, it should go to the main panel where all the created/invited-to events are shown
-  app.get('/', function(req, res) {
-    db.Events.findAll({ where: { id: }})
-  }; */
+  app.get("/signup", function (req, res) {
+    console.log(req.body);
+/*     db.User.findAll({}).then(function (dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    }); */
+    res.render("signup");
+  });
+
+  app.post("/signup", function(req, res) {
+    db.User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }).then(function() {
+      res.redirect(307, "/");
+    }).catch(function(err) {
+      console.log(err);
+      res.json(err);
+      // res.status(422).json(err.errors[0].message);
+    });
+  });
+
+  /*Login get function */
+  app.get("/", function (req, res) {
+    console.log(req.body);
+    db.User.findAll({}).then(function (dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
 
 
 
@@ -37,3 +70,29 @@ module.exports = function(app) {
     res.render("404");
   });
 };
+
+
+
+//Bring express into the file.
+const express = require('express');
+//Ask express to provide us with a new
+//instance of it's router module.
+const router = express.Router();
+const isAuthenticated = require('../middlewares/isAuthenticated');
+//A simple GET route
+router.get('/', isAuthenticated, function(req, res) {
+  //Using the handlebars view engine
+  //we tell it to render the
+  //welcome.hbs view, and give it
+  //an object containing a key called
+  //username with the value
+  //'Your nice name' so that the view can
+  //use it.
+  res.render('welcome.hbs', {
+    username: req.user.firstName
+  });
+});
+
+//Prepare the file to output our router.
+module.exports = router;
+
